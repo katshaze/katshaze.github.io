478c89146a30d1e421138d05c48f23ee185cc47f
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-05-02 23:20:40 +1000

    all links added, more styling, reviewed content

--- portfolio/animations.js
+++ portfolio/animations.js
@@ -4,19 +4,25 @@ $(document).ready(function() {
     e.preventDefault();
     $('html, body').animate({
     scrollTop: 0}, 1000);
-  })
+  });
+
+  $('#foot-nav').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+    scrollTop: 0}, 1000);
+  });
 
   $('#about-nav').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $("#about-heading").offset().top - 60
     }, 1000);
-  })
+  });
 
   $('#projects-nav').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $("#projects-heading").offset().top - 60
     }, 1000);
-  })
+  });
 });
--- portfolio/canvas.js
+++ portfolio/canvas.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
 
+  // scrolling animations
+  $('#home-nav').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+    scrollTop: 0}, 1000);
+  });
+
+  $('#foot-nav').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+    scrollTop: 0}, 1000);
+  });
+
+  $('#about-nav').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#about-heading").offset().top - 60
+    }, 1000);
+  });
+
+  $('#projects-nav').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#projects-heading").offset().top - 60
+    }, 1000);
+  });
+
+  // canvas
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
   const container = document.querySelector('.container');
@@ -30,7 +58,7 @@ $(document).ready(function() {
 
         ctx.beginPath();
         ctx.strokeStyle = `rgb(
-                      ${Math.floor(255 - (y * 4))}, 70,
+                      ${Math.floor(255 - (y * 4))}, 150,
                       ${Math.floor(255 - (x * 20))})`;
         if (leftToRight) {
           // draw  left to right line = \
@@ -58,8 +86,8 @@ $(document).ready(function() {
 
         ctx.beginPath();
         ctx.strokeStyle = `rgb(
-                      ${Math.floor(255 - ((y + 1) * 5))}, 0,
-                      ${Math.floor(255 - ((x + 1) * 20))})`;
+                      ${Math.floor(255 - ((y + 1) * 2))}, 0,
+                      ${Math.floor(255 - ((x + 1) * 15))})`;
         if (leftToRight) {
           // draw  left to right line = \
           ctx.moveTo(xOffset, yOffset);
@@ -86,7 +114,7 @@ $(document).ready(function() {
 
         ctx.beginPath();
         ctx.strokeStyle = `rgb(${Math.floor(255 - ((x + 1) * 5))},
-        ${Math.floor(255 - ((y + 1) * 20))}, 0)`;
+        ${Math.floor(255 - ((y + 1) * 5))}, 0)`;
         if (vertical) {
           // draw vertical line = |
           ctx.moveTo(xOffset, yOffset);
@@ -102,11 +130,39 @@ $(document).ready(function() {
     }
   }
 
+  function generateHorVertLines2(ctx, tileSize, w, h) {
+   ctx.clearRect(0, 0, w, h);
+
+    for (let y = 0; y <= (h / tileSize); y++) {
+      for (let x = 0; x <= (w / tileSize); x++) {
+        const vertical = Math.random() >= .5;
+        const xOffset = x * tileSize;
+        const yOffset = y * tileSize;
+
+        ctx.beginPath();
+        ctx.strokeStyle = `rgb(30,
+        ${Math.floor(255 - ((y + 1) * 5))}, ${Math.floor(255 - ((x + 1) * 5))})`;
+        if (vertical) {
+          // draw vertical line = |
+          ctx.moveTo(xOffset, yOffset);
+          ctx.lineTo(xOffset, yOffset + tileSize);
+        } else {
+          // draw horizontal line = --
+          ctx.moveTo(xOffset, yOffset);
+          ctx.lineTo(xOffset + tileSize, yOffset);
+        }
+
+        ctx.stroke();
+      }
+    }
+  }
+
+
   // generate pattern initially & on refresh
   generateOverlapDiagonals(ctx, tileSize, w, h);
 
   // do it againnnnnn on each click, alternating
-  $('body').add('a').on('click', function(){
+  $('body, a').on('click', function(){
     if (nextUp === 'a') {
       generateOverlapDiagonals(ctx, tileSize, w, h);
       nextUp = 'b';
@@ -117,12 +173,9 @@ $(document).ready(function() {
       generateDiagonals(ctx, tileSize, w, h);
       nextUp = 'd';
     } else if (nextUp === 'd') {
-      generateHorVertLines(ctx, tileSize, w, h);
+      generateHorVertLines2(ctx, tileSize, w, h);
       nextUp = 'a';
     }
   });
 
-  $('a').on('click', function() {
-    console.log('link tag was clicked');
-  })
 }); // end of doc ready
