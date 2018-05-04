c9c61a2bae0c61bb29e4662c0978356e27ab2518
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-05-04 21:04:33 +1000

    more responsiveness

--- js/animations.js
+++ js/animations.js
@@ -15,14 +15,14 @@ $(document).ready(function() {
   $('#about-nav').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
-        scrollTop: $("#about-heading").offset().top - 70
+        scrollTop: $("#about-heading").offset().top - 64
     }, 1000);
   });
 
   $('#projects-nav').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
-        scrollTop: $("#projects-heading").offset().top - 70
+        scrollTop: $("#projects-heading").offset().top - 64
     }, 1000);
   });
 });
