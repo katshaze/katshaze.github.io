0fd08b5b5e84713d3f827bc5561a6d8f1d51eb50
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-05-07 16:06:55 +1000

    finally fixed that freakin scroll glitch

--- js/canvas.js
+++ js/canvas.js
@@ -1,32 +1,5 @@
 $(document).ready(function() {
 
-  // scrolling animations
-  $('#home-nav').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-    scrollTop: 0}, 1000);
-  });
-
-  $('#foot-nav').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-    scrollTop: 0}, 1000);
-  });
-
-  $('#about-nav').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $("#about-heading").offset().top - 60
-    }, 1000);
-  });
-
-  $('#projects-nav').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $("#projects-heading").offset().top - 60
-    }, 1000);
-  });
-
   // canvas
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
